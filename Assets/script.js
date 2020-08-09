51ff0f9443b73341613d9588a8b00c72e6d5bfd1
vsteere <vssteere@gmail.com>
2020-08-09 15:05:00 -0500

    adding the current weather icon and UV index

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+//jQuery code to make it run only after everything loads
+
 $(document).ready(function () {
 
     var currentWeather = $("#currentWeather");
@@ -54,7 +56,6 @@ $(document).ready(function () {
         //this pulls the city name of whatever button we click on
         var thisCity = $(this).attr("data-city");
         //data pull via api
-        // var weatherPull = "http://api.openweathermap.org/data/2.5/forecast?q=" + thisCity + "&appid=5290a147bd4c081007c34f429776aca3";
         var weatherPull = "http://api.openweathermap.org/data/2.5/weather?q=" + thisCity + "&appid=5290a147bd4c081007c34f429776aca3";
 
         $.ajax({
@@ -62,30 +63,28 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
 
-            console.log("hi we are here");
-
-            console.log(response.cod);
             //checking if the data pulls. This works
             console.log(response);
 
             //pull the specific object from the data to the dashboard
-
             console.log(response.weather[0].icon)
             var cityName = response.name;
             var cityNamedisp = $("<h2>");
-            //image not working need to fix later
-            var currentImage = $("<img>");
-            currentImage.attr("src", "http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png")
-            console.log(moment().format("lll"));
-            //this will keep only one city's data in thh box
+            //Pulling image from data to the page
+            var currentImage = response.weather[0].icon;
+            var currentImagedisp = "http://openweathermap.org/img/wn/" + currentImage + ".png";
+            var currentImagedispaddy = $("<img>").attr("src", currentImagedisp);
+            //this will keep only one city's data in the box
             currentWeather.empty();
             //learned from a tutor the syntax for the below; city name and current date
-            cityNamedisp.text(`${cityName}  ${moment().format("ll")} ${currentImage} `);
-            currentWeather.append(cityNamedisp);
+            cityNamedisp.text(`${cityName}  ${moment().format("ll")}  `);
 
-            //pulling the data for temp and converting it to Fahrenheit. NEED TO FIX THE ROUNDING
+            currentWeather.append(cityNamedisp);
+            currentWeather.append(currentImagedispaddy);
+
+            //pulling the data for temp, converting it from Kelvin Fahrenheit, and rounding to 2 decimal points
             var currTemp = response.main.temp;
-            var currTempfarh = (response.main.temp - 273.15) * 1.80 + 32;
+            var currTempfarh = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2);
             var tempP = $("<p>");
             tempP.text(`Temperature:  ${currTempfarh} degrees Fahrenheit`)
             currentWeather.append(tempP);
@@ -104,39 +103,54 @@ $(document).ready(function () {
             currentWeather.append(windP);
 
 
-            //code to pull the UV index
-
-            var latCoord = response.coord.lat;
-            var longCoord = response.coord.lon;
-
-            var uvPull = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latcoord + "&lon=" + longCoord;
-
-
-
-            $.ajax({
-
-                url: uvPUll,
-                method: "GET"
-
-            }).then(function (response) {
-
-                console.log(response);
-
-            });
 
 
         });
+        $.ajax({
+            url: weatherPull,
+            method: "GET"
+        }).then(function (response) {
+
+            //checking if the data pulls. This works
+            console.log(response);
+
+            //the longitude and latitude coordinates data for the UV index pull
+            var currCitylat = response.coord.lat;
+            var currCitylong = response.coord.lon;
+
+            //defining the API URL for the UV index pull
+            var uvPull = "http://api.openweathermap.org/data/2.5/uvi?appid=5290a147bd4c081007c34f429776aca3&lat=" + currCitylat + "&lon=" + currCitylong;
+
+
+            //AJAX call for the UV index using the latitude and longitude from above
+            $.ajax({
+                url: uvPull,
+                method: "GET"
+            }).then(function (response) {
+
+                var curUVindex = response.value;
+                var curUVindexp = $("<p>");
+                curUVindexp.text(`UV Index = ${curUVindex}`)
+                //this checks if the API is pulling; this works
+                console.log(curUVindex)
+                currentWeather.append(curUVindexp);
+            })
+
+
+        })
+
 
 
 
     }
 
-    function pastWeather () {
+
+
+    function pastWeather() {
         //this pulls the city name of whatever button we click on
         var thisCity = $(this).attr("data-city");
         var pastWeather = "http://api.openweathermap.org/data/2.5/forecast?q=" + thisCity + "&appid=5290a147bd4c081007c34f429776aca3";
 
-        
 
 
 
@@ -152,4 +166,6 @@ $(document).ready(function () {
     //on click event handler when the city btn class button is clicked it runs the getWeather function for that city
     $(document).on("click", ".city-btn", getWeather);
 
+
+    //these are the closing brackets for document.ready code up at the top
 })
